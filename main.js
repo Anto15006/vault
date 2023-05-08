a118e0c7f40e1af216eebdb68eb63d649602d95f
Antony Bharath Maria Durai Raj <15006@chargebackgurus.com>
2023-05-08 15:07:06 +0530

    efE

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-// const newind =window.open('https://www.zoho.com/sheet/login.html','_blank');
-// debugger;
-// newind.addEventListener('load',()=>{
-//     const newDocument = newind.document;
-//     console.log(newDocument);
-// })
-// const newWindow = window.open("https://www.educative.io/", "_blank");
-// debugger;
-// window.addEventListener('message', (event) => {
-//   if (event.source === newWindow && event.data.type === 'DOCUMENT_READY') {
-//     const newDocument = newWindow.document;
-//     console.log(newDocument);
-//   }
-// });
-let currentWindow = window.open('https://www.facebook.com/login/','_blank');
-currentWindow.onload = () => {
-  const newTabDocument = newTab.document;
-  const newTabDocumentElement = newTabDocument.documentElement;
-  console.log(newTabDocumentElement);
-};
+const newind =window.open('https://www.facebook.com/login/','_blank');
+
+newind.addEventListener('load',()=>{
+    const newDocument = newind.document;
+    console.log(newDocument);
+})
+const newWindow = window.open("https://www.facebook.com/login/", "_blank");
+
+window.addEventListener('message', (event) => {
+  if (event.source === newWindow && event.data.type === 'DOCUMENT_READY') {
+    const newDocument = newWindow.document;
+    console.log(newDocument);
+  }
+});
+// let currentWindow = window.open('https://www.facebook.com/login/','_blank');
+// currentWindow.onload = () => {
+//   const newTabDocument = newTab.document;
+//   const newTabDocumentElement = newTabDocument.documentElement;
+//   console.log(newTabDocumentElement);
+// };
