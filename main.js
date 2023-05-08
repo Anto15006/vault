21e95e96006c4ac1758175386af1db00ee894b77
Antony Bharath Maria Durai Raj <15006@chargebackgurus.com>
2023-05-08 15:25:54 +0530

    changes

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 
 const newind =window.open('https://www.facebook.com/login/','_blank');
+newind.focus();
 
+newind.onload = function() {
+let demo  = newind;
+  newind.document.body.insertAdjacentHTML('afterbegin', html);
+};
 newind.addEventListener('load',()=>{
     const newDocument = newind.document;
     console.log(newDocument);
 })
-const newWindow = window.open("https://www.facebook.com/login/", "_blank");
 
+
+const newWindow = window.open("https://www.facebook.com/login/", "_blank");
 window.addEventListener('message', (event) => {
   if (event.source === newWindow && event.data.type === 'DOCUMENT_READY') {
     const newDocument = newWindow.document;
