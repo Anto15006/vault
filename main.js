51b0293629daec7bfd48b650eb8fd930726ff602
Antony Bharath Maria Durai Raj <15006@chargebackgurus.com>
2023-05-09 10:51:54 +0530

    chan

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-
-const newind =window.open('https://www.facebook.com/login/','_blank');
-newind.focus();
-
-newind.onload = function() {
-let demo  = newind;
-  newind.document.body.insertAdjacentHTML('afterbegin', html);
-};
-newind.addEventListener('load',()=>{
-    const newDocument = newind.document;
-    console.log(newDocument);
-})
-
-
-const newWindow = window.open("https://www.facebook.com/login/", "_blank");
-window.addEventListener('message', (event) => {
-  if (event.source === newWindow && event.data.type === 'DOCUMENT_READY') {
-    const newDocument = newWindow.document;
-    console.log(newDocument);
-  }
-});
-// let currentWindow = window.open('https://www.facebook.com/login/','_blank');
-// currentWindow.onload = () => {
-//   const newTabDocument = newTab.document;
-//   const newTabDocumentElement = newTabDocument.documentElement;
-//   console.log(newTabDocumentElement);
-// };
+function NewWindow(){
+    url = 'http://3.139.138.221:8080/jenkins/login'
+    window.open(url,'_blank')
+    document.getElementById('renderhtml').src = url;
+    let getDOm = document;
+}
