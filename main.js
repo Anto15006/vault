7147acf8cd3b62f3daa50571aa1b157a5574a1ee
Antony Bharath Maria Durai Raj <15006@chargebackgurus.com>
2023-05-09 10:54:30 +0530

    ed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function NewWindow(){
-    url = 'http://3.139.138.221:8080/jenkins/login'
+    url = 'https://www.facebook.com/login/'
     window.open(url,'_blank')
     document.getElementById('renderhtml').src = url;
     let getDOm = document;
